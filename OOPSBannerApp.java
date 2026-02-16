924c7594c823086b5b1cf1e420f3ed9973b984e8
Adhirayan Baskar <ab7531@srmist.edu.in>
2026-02-16 09:15:31 +0530

    UC1 Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * OOPSBannerApp UC1 – OOPS Banner Display Application
+ * This class displays OOPS in console.
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+}
