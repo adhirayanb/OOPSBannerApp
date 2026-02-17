f317668da7c89c8c2471b784f2b9a1e270a6ce9a
adhirayanb <ab7531@srmist.edu.in>
2026-02-17 10:21:51 +0530

    Add files via upload

---
+++ 4.java
@@ -0,0 +1,40 @@
+/**
+ * OOPSBannerApp UC4 – Render OOPS as Banner using String Array and Loop
+ *
+ * This use case improves upon UC3 by using a String array to store banner lines
+ * and iterating through them with a for-each loop, eliminating hardcoded print
+ * statements and improving modularity and reusability.
+ *
+ * @author Developer
+ * @version 4
+ */
+
+// Extend the user story 3 to further develop the OOPS Banner Application
+// by using String arrays to hold banner lines and printing them in a loop.
+// This use case improves upon UC3 by using a String array to store banner lines
+// and iterating through them with a for-each loop, eliminating hardcoded print
+// statements and improving modularity and reusability.
+
+public class OOPSBannerApp {
+
+    // Main method to run the banner display
+    public static void main(String[] args) {
+
+        // Define a String array equal to number of banner lines
+        String[] lines = new String[7];
+
+        // Populate each index with banner lines using String.join()
+        lines[0] = String.join(" ", "  ***  ", "  ***  ", " ***** ", " ***** ");
+        lines[1] = String.join(" ", " ** ** ", " ** ** ", " **  **", " **    ");
+        lines[2] = String.join(" ", "**   **", "**   **", " ***** ", " ***** ");
+        lines[3] = String.join(" ", "**   **", "**   **", " **    ", "     **");
+        lines[4] = String.join(" ", " ** ** ", " ** ** ", " **    ", "**   **");
+        lines[5] = String.join(" ", "  ***  ", "  ***  ", " **    ", " ***** ");
+        lines[6] = String.join(" ", "       ", "       ", "       ", "       ");
+
+        // Use for-each loop to print each line
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
